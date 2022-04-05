8342173f798e9a491204e6432693ff6eccd21ec0
RaizigueFurlan <102595658+RaizigueFurlan@users.noreply.github.com>
2022-04-05 09:57:25 -0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ATIVIDADE
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void fechar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nome_usuario_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+
+        private void btconfir_Click(object sender, RoutedEventArgs e)
+        {
+
+            string senha = pbsenha.Password.ToString();
+            string usuario = txyusuario.Text;
+
+            if (usuario == "raizigue" && senha =="123456")
+            {
+                T2 obj = new T2();
+                obj.ShowDialog();
+            }
+
+          
+
+            txyusuario.Clear(); 
+            pbsenha.Clear();    
+
+            
+
+        }
+
+
+        private void pbsenha_Click_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ T2.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ATIVIDADE
+{
+    /// <summary>
+    /// Lógica interna para T2.xaml
+    /// </summary>
+    public partial class T2 : Window
+    {
+        public T2()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_adicao_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(tb_val_a.Text);
+            double b = Convert.ToDouble(tb_valb.Text);
+            double re = a + b;
+            tb_result.Text = re.ToString();
+        }
+
+        private void bt_sibt_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(tb_val_a.Text);
+            double b = Convert.ToDouble(tb_valb.Text);
+            double re = a - b;
+            tb_result.Text = re.ToString();
+        }
+
+        private void bt_div_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(tb_val_a.Text);
+            double b = Convert.ToDouble(tb_valb.Text);
+            double re = a * b;
+            tb_result.Text = re.ToString();
+        }
+
+        private void bt_div1_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(tb_val_a.Text);
+            double b = Convert.ToDouble(tb_valb.Text);
+            double re = a / b;
+            tb_result.Text = re.ToString();
+        }
+
+        private void tb_result_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void bt_apagar_Click(object sender, RoutedEventArgs e)
+        {
+            tb_result.Clear();
+            tb_val_a.Clear();       
+            tb_valb.Clear();
+            
+        }
+    }
+}
